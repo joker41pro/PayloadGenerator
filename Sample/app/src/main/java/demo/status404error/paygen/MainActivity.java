cc067265341f88223d828f3f06f35a1b9961bec5
Kervy Calub <kervycalub1@gmail.com>
2019-07-14 22:44:47 +0800

    Add Files

---
+++ Sample/app/src/main/java/demo/status404error/paygen/MainActivity.java
@@ -0,0 +1,14 @@
+package demo.status404error.paygen;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
