2568c14e13a721ccfa343f8361fd4af8172ff0eb
status404error <kervycalub1@gmail.com>
2019-07-17 08:45:49 +0800

    for 1.2

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
 # PayloadGenerator
+
+For easier generating of payload for SSH. Based on the HTTP Injector of Evozi. Credits to them.
+
+## Implementation
+
+#### Add this to your _root_ build.gradle
+
+``` gradle
+allprojects { 		
+   repositories {
+       ... 			
+       maven { url 'https://jitpack.io' }
+   } 	
+}
+
+```
+
+#### Add this to your dependencies
+```
+dependencies { 	 
+    compile 'com.github.status404error:PayloadGenerator:1.2' 	
+}
+```
+
+I suggest to use **Switch** to show and to enable and disable the usage of custom payload.
+Also take note that you must define your SharedPreference that you use for custom payload enabling and disabling 
+
+Initialize PayloadGenerator
+
+```java
+private PayloadGenerator payloadGenerator;
+```
+
+On your OnCreate or OnCreateView (for FragmentActivity)
+
+```java
+Switch switch = (Switch) ...
+payloadGenerator = new PayloadGenerator(this, sharedPrefs);
+
+// add switch on the PayloadGenerator
+// so when cancel button is clicked on dialog,
+// it will automatically uncheck the switch
+payloadGenerator.addPayloadSwitch(switch);
+
+//add custom proxy
+payloadGenerator.setProxyKey("use_custom_proxy","generated_proxy");
+
+```
+
+On the **custom proxy**, you must add the boolean key used on SharedPreference for enabling custom proxy and then the string key
+Example: on your PreferenceActivity you used CheckBoxPreference for Custom Proxy, the key for that CheckBoxPreference is your boolean key for enabling custom proxy, same timg for the EditTextPreferece for adding your custom proxy.
+
+
+```java
+
+if(ischeck){
+   //enable custom payload and show PayloadGenerator
+   editor.putBoolean("use_custom_payload",true).apply();
+   //add condition for switches 
+   //if use ssh or sni
+   if(ssh_radio.isChecked()){
+      payloadGenerator.showForSSH("generated_ssh");
+   }else{
+      payloadGenerator.showForSNI("generated_sni");
+   }
+
+}else{
+//disable custom payload
+editor.putBoolean("use_custom_payload",false).apply();
+				}
+```
+
+If you will not declare key for custom proxy, the Option on the **PayloadGenerator** for custom proxy will be useless
+
+Versions:
+
+* 1.2
+ - Fixed gradle issues
+
+
+* 1.1
+ - Fixed some issues
+
+
+* 1.0
+ - Initial Release
--- app/src/main/java/demo/status404error/paygen/MainActivity.java
+++ app/src/main/java/demo/status404error/paygen/MainActivity.java
@@ -2,12 +2,105 @@ package demo.status404error.paygen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.status404error.paygen.PayloadGenerator;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.RadioButton;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
+import android.preference.*;
+import demo.status404error.paygen.R;
+import android.app.*;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener
+{
+
+	private PayloadGenerator paygen;
+	private Switch paygen_switch;
+	private RadioButton ssh_radio, sni_radio;
+	private Button show_btn;
+	private SharedPreferences prefs;
+	private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		//initialize SharedPreference
+		this.prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		this.editor = prefs.edit();
+		/*initialize PayloadGenerator
+		 * do not use getApplicationContext() 
+		 * when initializing context for PayloadGenerator
+		 * else it will not work
+		 */
+		 this.paygen = new PayloadGenerator(this, prefs);
+		 this.paygen_switch = (Switch) findViewById(R.id.paygen_switch);
+		 this.ssh_radio = (RadioButton) findViewById(R.id.ssh_radio);
+		 this.sni_radio = (RadioButton) findViewById(R.id.sni_edit);
+		 this.show_btn = (Button) findViewById(R.id.show_btn);
+		 this.ssh_radio.setChecked(true);
+		 this.paygen_switch.setOnCheckedChangeListener(this);
+		 this.show_btn.setOnClickListener(this);
+		 // add switch on the PayloadGenerator
+		 // so when cancel button is clicked on dialog,
+		 // it will automatically uncheck the switch
+		 paygen.addPayloadSwitch(paygen_switch);
+		 //add custom proxy
+		 paygen.setProxyKey("use_custom_proxy","generated_proxy");
     }
+	
+	@Override
+	public void onClick(View view)
+	{
+		switch(view.getId()){
+			case R.id.show_btn:
+				AlertDialog.Builder adb = new AlertDialog.Builder(this);
+				adb.setTitle("Result");
+				if(prefs.getBoolean("use_custom_payload",false)==true){
+					if(ssh_radio.isChecked()){
+						if(prefs.getBoolean("use_custom_proxy", false)==true){
+							adb.setMessage("Payload:\n"+prefs.getString("generated_ssh","")+"\nProxy:\n"+prefs.getString("generated_proxy",""));
+						}else{
+							adb.setMessage(prefs.getString("generated_ssh",""));
+						}
+						
+					}else{
+						adb.setMessage(prefs.getString("generated_sni",""));
+					}
+				}else{
+					adb.setMessage("Custom Payload Disabled");
+				}
+				
+				adb.setPositiveButton("Close",null);
+				adb.create().show();
+				break;
+		}
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton widget, boolean ischeck)
+	{
+		switch(widget.getId()){
+			case R.id.paygen_switch:
+				if(ischeck){
+					//you must commit the boolean here for custom payload to true
+					editor.putBoolean("use_custom_payload",true).apply();
+					//add condition for switches 
+					//if use ssh or sni
+					if(ssh_radio.isChecked()){
+						paygen.showForSSH("generated_ssh");
+					}else{
+						paygen.showForSNI("generated_sni");
+					}
+				}else{
+					//set custom payload to false
+					editor.putBoolean("use_custom_payload",false).apply();
+				}
+				break;
+		}
+		// TODO: Implement this method
+	}
 }
